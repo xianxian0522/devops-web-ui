e1c4d503f48c61aae69f45e4f3b6f818026613eb
suixian.gong <suixian.gong@sumscope.com>
2021-07-09 16:45:23 +0800

    icon

--- src/components/AppLayout.vue
+++ src/components/AppLayout.vue
@@ -17,7 +17,7 @@
             <template v-if="!item.children">
               <a-menu-item :key="item.path">
                 <span>
-<!--                  <icon-font :type="item.icon" />-->
+                  <icon-font :type="item.icon" />
                   <router-link :to="{path: '/app/' + appId + '/' + item.path, }">{{ item.name }}</router-link>
                 </span>
               </a-menu-item>
@@ -25,12 +25,12 @@
             <template v-else>
               <a-sub-menu :key="item.path">
                 <template #title>
-                  <!--                  <icon-font :type="item.icon" />-->
+                  <icon-font :type="item.icon" />
                   <span>{{ item.name }}</span>
                 </template>
                 <a-menu-item v-for="t in item.children" :key="t.path">
                   <span>
-<!--                  <icon-font :type="t.icon" />-->
+                  <icon-font :type="t.icon" />
                   <router-link :to="{path: '/app/' + appId + '/' + t.path, }" >{{ t.name }}</router-link>
                   </span>
                 </a-menu-item>
@@ -52,9 +52,15 @@ import { useRoute, } from "vue-router";
 import { BarItem } from "@/utils/response";
 import { reactive, ref, toRefs, watch } from "vue";
 import appInfoRepositories from "@/composable/appInfoRepositories";
+import { createFromIconfontCN } from "@ant-design/icons-vue";
+
+const IconFont = createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_2663750_agiifc2fo96.js'
+})
+
 export default {
   name: "AppLayout",
-  components: { CommonLayoutHeader },
+  components: { CommonLayoutHeader, IconFont },
   setup() {
     const route = useRoute()
     const url = route.path.split('/')
@@ -64,15 +70,15 @@ export default {
       selectedKeysMenu: [url[3]],
     })
     const bar = ref<BarItem[]>([
-      {id: 1, icon: '', path: 'index', name: '应用总览'},
-      {id: 2, icon: '', path: 'members', name: '应用成员'},
-      {id: 3, icon: '', path: 'setup', name: '应用设置',
+      {id: 1, icon: 'icon-app', path: 'index', name: '应用总览'},
+      {id: 2, icon: 'icon-member', path: 'members', name: '应用成员'},
+      {id: 3, icon: 'icon-app-set', path: 'setup', name: '应用设置',
         children: [
-          {id: 4, icon: '', path: 'set-information', name: '基本信息'},
-          {id: 5, icon: '', path: 'set-cluster', name: '集群设置'},
+          {id: 4, icon: 'icon-info', path: 'set-information', name: '基本信息'},
+          {id: 5, icon: 'icon-cluster', path: 'set-cluster', name: '集群设置'},
         ],
       },
-      {id: 6, icon: '', path: 'host-details', name: '机器详情'},
+      {id: 6, icon: 'icon-host', path: 'host-details', name: '机器详情'},
     ])
 
     watch(() => route.path, value => {
--- src/components/BizLayout.vue
+++ src/components/BizLayout.vue
@@ -27,7 +27,7 @@
             <template v-if="!item.children">
               <a-menu-item :key="item.path">
                 <span>
-<!--                  <icon-font :type="item.icon" />-->
+                  <icon-font :type="item.icon" />
                   <router-link :to="{path: '/biz/' + item.path, query: { bizId: bizId }}">{{ item.name }}</router-link>
                 </span>
               </a-menu-item>
@@ -35,12 +35,12 @@
             <template v-else>
               <a-sub-menu :key="item.path">
                 <template #title>
-<!--                  <icon-font :type="item.icon" />-->
+                  <icon-font :type="item.icon" />
                   <span>{{ item.name }}</span>
                 </template>
                 <a-menu-item v-for="t in item.children" :key="t.path">
                   <span>
-<!--                  <icon-font :type="t.icon" />-->
+                  <icon-font :type="t.icon" />
                   <router-link :to="{path: '/biz/' + t.path,  query: { bizId: bizId }}" >{{ t.name }}</router-link>
                   </span>
                 </a-menu-item>
@@ -62,11 +62,17 @@ import { useRoute, useRouter } from "vue-router";
 import bizRepositories from "@/composable/bizRepositories";
 import { onMounted, reactive, ref, toRefs, watch } from "vue";
 import { BarItem } from "@/utils/response";
+import { createFromIconfontCN } from "@ant-design/icons-vue";
+
+const IconFont = createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_2663750_agiifc2fo96.js'
+})
 
 export default {
   name: "BizLayout",
   components: {
     CommonLayoutHeader,
+    IconFont,
   },
   setup() {
     const route = useRoute()
@@ -80,15 +86,15 @@ export default {
       selectedKeysMenu: [url[2]],
     })
     const bar = ref<BarItem[]>([
-      {id: 1, icon: '', path: 'index', name: '业务总览'},
-      {id: 2, icon: '', path: 'members', name: '业务成员'},
-      {id: 3, icon: '', path: 'setup', name: '设置',
+      {id: 1, icon: 'icon-biz', path: 'index', name: '业务总览'},
+      {id: 2, icon: 'icon-member', path: 'members', name: '业务成员'},
+      {id: 3, icon: 'icon-set', path: 'setup', name: '设置',
         children: [
-          {id: 4, icon: '', path: 'set-information', name: '基本信息'},
-          {id: 5, icon: '', path: 'app-settings', name: '应用设置'},
+          {id: 4, icon: 'icon-info', path: 'set-information', name: '基本信息'},
+          {id: 5, icon: 'icon-app-set', path: 'app-settings', name: '应用设置'},
         ],
       },
-      {id: 6, icon: '', path: 'host-details', name: '机器详情'},
+      {id: 6, icon: 'icon-host', path: 'host-details', name: '机器详情'},
     ])
 
     const filterOptionBiz = (input: string, option: any) => {
